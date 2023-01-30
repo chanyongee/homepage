81a267113b5e97d1dbd60d6c275830e5f8da6459
Chanyeong Kim <cykim@neosapience.com>
2023-01-30 22:42:01 +0900

    chore: dist

--- dist/_nuxt/static/1675085356/story/disaster/intro/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675085356",layout:"default",error:a,state:{breadcrumbs:[]},serverRendered:true,routePath:"\u002Fstory\u002Fdisaster\u002Fintro",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}}}}(null));
