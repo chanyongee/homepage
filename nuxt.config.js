74025e2a44132b26732cc2de223dbd1c7865dd89
Chanyeong Kim <cykim@neosapience.com>
2023-01-30 22:41:11 +0900

    chore: router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: 'static',
   server: { port: 3001 },
   router: {
-    base: '/',
+    base: '/homepage/',
   },
   styleResources: {
     scss: ['./assets/scss/index.scss'],
