b75c263ee4a3b21cc7ee6cd9687959d7a08e6e42
Chanyeong Kim <cykim@neosapience.com>
2023-02-18 00:13:28 +0900

    feat: mobile ui dist

--- dist/_nuxt/a76832d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{446:function(e,n,t){"use strict";t.r(n),n.default={title:"수질측정기기관리",descriptions:["수질TMS(Tele Monitoring System) 측정기기 관리대행","수질계측기기 관리대행"],diagram:"수질측정기기절차도.png",diagramName:"수질측정기기 관리대행 절차도"}}}]);
---
+++ dist/_nuxt/static/1676646744/story/disaster/pre/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/story/disaster/pre", {data:[{data:{title:"사전재해영향성검토",descriptions:["자연재해에 영향을 미치는 행정계획으로 인한 재해 유발요인을 예측ㆍ분석하여 이에 대한 대책을 마련하는 제도"]}}],fetch:{},mutations:[]});
